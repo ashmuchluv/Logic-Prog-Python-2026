6b162cfc0cdcddbf51b95d903a14623980d505dd
Gabriel Rabelo <gabriel.rabelo1704@gmail.com>
2026-09-16 08:42:39 -0300

    Add interval check for numbers between 10 and 50

---
+++ Intervalo entre 10 a 50.py
@@ -0,0 +1,8 @@
+print("Intervalo entre 10 a 50")
+
+numero = float(input ("Insira um Número: \n"))
+
+if numero >=10 and numero <=50:
+  print("Faz parte do intervalo")
+else:
+  print("Número fora do intervalo")
