62ae9b84a44704dc7a0689a6c60d95eaacceda74
Gabriel Rabelo <gabriel.rabelo1704@gmail.com>
2026-09-16 08:40:05 -0300

    Add script to determine if a number is positive, negative, or zero

---
+++ positivo ou negativo.py
@@ -0,0 +1,11 @@
+# definir se o número é positivo, negativo, ou zero
+print("Positivo, negativo ou zero")
+
+numero = int (input("Digite um número: "))
+
+if numero >0:
+  print("Positivo")
+elif numero <0:
+  print("Negativo")
+else:
+  print("Zero")
