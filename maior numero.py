b4edc601da5ffc3530e04e62b446a834b1d1b289
Gabriel Rabelo <gabriel.rabelo1704@gmail.com>
2026-09-16 08:40:42 -0300

    Add program to determine the larger of two numbers
    Implement a program to find the larger of two numbers based on user input.

---
+++ maior numero.py
@@ -0,0 +1,11 @@
+# define o maior número escolhido
+print ("Maior entre 2")
+
+a = int (input("insira o primeiro número: "))
+b = int (input("insira o segundo número: "))
+
+if a > b:
+  print("Maior: ", a)
+
+elif b > a:
+  print("Maior: ",b)
