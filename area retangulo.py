ce28cf284accbea59ee92283e331c0773d4cedbd
Gabriel Rabelo <gabriel.rabelo1704@gmail.com>
2026-09-16 08:39:01 -0300

    Add area calculation for rectangle

---
+++ area retangulo.py
@@ -0,0 +1,9 @@
+# area de um retangulo
+print ("Perímetro do Retângulo")
+
+lado1 = float (input ("lado 1: "))
+lado2 = float (input ("lado 2: "))
+
+area = (lado1 + lado2) * 2
+
+print ("Área: ", area)
